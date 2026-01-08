c3d09e0ce2017d529cbdab45f1eef9385acac3c3
bop@festgroup.in <bop@festgroup.in>
2026-01-08 16:22:07 +0530

    chnges in json

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -44,7 +44,7 @@ app.get(
   }
 );
 
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = (process.env.PORT) || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
